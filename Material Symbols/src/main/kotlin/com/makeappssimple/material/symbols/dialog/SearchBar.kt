34aa75d54dda21471b688f6d040c4d6e51c1fe9a
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-10-06 22:06:42 +0530

    [TECH] searchbar

--- Material Symbols/src/main/kotlin/com/makeappssimple/material/symbols/dialog/SearchBar.kt
+++ Material Symbols/src/main/kotlin/com/makeappssimple/material/symbols/dialog/SearchBar.kt
@@ -1,46 +1,35 @@
 package com.makeappssimple.material.symbols.dialog
 
 import com.intellij.ui.SearchTextField
-import java.awt.BorderLayout
-import javax.swing.JPanel
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
 internal class SearchBar(
     private val onSearchTextUpdate: (String) -> Unit,
-) : JPanel() {
-    private val searchTextField = SearchTextField()
-
+) : SearchTextField() {
     init {
-        initUI()
         initListeners()
     }
 
-    private fun initUI() {
-        layout = BorderLayout()
-
-        add(searchTextField, BorderLayout.NORTH)
-    }
-
     private fun initListeners() {
-        searchTextField.addDocumentListener(
+        addDocumentListener(
             object : DocumentListener {
                 override fun insertUpdate(
                     e: DocumentEvent?,
                 ) {
-                    onSearchTextUpdate(searchTextField.text)
+                    onSearchTextUpdate(text)
                 }
 
                 override fun removeUpdate(
                     e: DocumentEvent?,
                 ) {
-                    onSearchTextUpdate(searchTextField.text)
+                    onSearchTextUpdate(text)
                 }
 
                 override fun changedUpdate(
                     e: DocumentEvent?,
                 ) {
-                    onSearchTextUpdate(searchTextField.text)
+                    onSearchTextUpdate(text)
                 }
             },
         )
